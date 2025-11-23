714c0e3e3d688d2d0c569a82640d987554a768ee
Nistec <julian.larsen07@gmail.com>
2025-11-23 10:44:19 +0100

    scrapeAhead fixed

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -29,9 +29,9 @@ function handleToggle(val) {
 <template>
   <div>
     <Navbar :show-menu="menuOpen" @toggle="handleToggle" />
-    <div class="container" :style="{ paddingTop: menuOpen && !isMobile ? '4rem' : '0', 
-                                     paddingBottom: menuOpen && isMobile ? '4rem' : '0', 
-                                     paddingTop: isMobile ? '1rem' : '0'}">
+    <div class="container" :style="{ paddingTop: menuOpen ? (isMobile ? '1rem' : '2rem' ) : '0', 
+                                     paddingBottom: menuOpen ? (isMobile ? '4rem' : '0rem' ) : '0', 
+                                     }">
       <RouterView/>
     </div>
   </div>
